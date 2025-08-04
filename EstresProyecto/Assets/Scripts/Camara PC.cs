8c580b55ed018b96bb8a4af3caa37caff60fc914
Miguellc0 <miguel1asturias@gmail.com>
2025-08-03 18:33:41 -0600

    cinematica

---
+++ EstresProyecto/Assets/Scripts/Camara PC.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CamaraPC : MonoBehaviour
+{
+    public PlayableDirector timelineDirector; // Asigna el PlayableDirector (Timeline) desde el Inspector
+
+    private XRGrabInteractable grabInteractable;
+
+    void Awake()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.AddListener(OnGrabSilla);
+        }
+    }
+
+    private void OnGrabSilla(SelectEnterEventArgs args)
+    {
+        if (timelineDirector != null)
+        {
+            timelineDirector.Play();
+        }
+
+        // Si quieres que el jugador ya no pueda soltar la silla:
+        // grabInteractable.enabled = false;
+    }
+
+    private void OnDestroy()
+    {
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.RemoveListener(OnGrabSilla);
+        }
+    }
+}
--- EstresProyecto/Assets/Scripts/DesactivarLuz.cs
+++ EstresProyecto/Assets/Scripts/DesactivarLuz.cs
@@ -6,7 +6,7 @@ public class DesactivarLuz : MonoBehaviour
 {
     public string tagObjetivo = "Player";     // Cambia si necesitas otro tag
     public GameObject[] objetosADesactivar;   // Arrastra aquí los 5 objetos desde el inspector
-    public GameObject FondoPC;
+    public GameObject[] objetosActivar;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -19,7 +19,14 @@ public class DesactivarLuz : MonoBehaviour
                     obj.SetActive(false);
                 }
             }
-            FondoPC.SetActive(true);
+
+            foreach (GameObject obj in objetosActivar)
+            {
+                if (obj != null)
+                {
+                    obj.SetActive(true);
+                }
+            }
             // Si no quieres que vuelva a activarse, puedes desactivar este objeto también:
             // gameObject.SetActive(false);
         }
